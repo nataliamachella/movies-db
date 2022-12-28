d1eda8de2f42b9e07cd8b9fa9b2f969f8f7e5cc0
nataliamachella <nataliamachella@gmail.com>
2022-12-28 14:36:05 -0300

    .

--- api/index.js
+++ api/index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/api/v1", routes);
+
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movies-db-sand.vercel.app/";
+const baseURL = "https://movies-db-sand.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
