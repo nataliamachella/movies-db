369e0faf869366b386cf537e7fb0126757840402
nataliamachella <nataliamachella@gmail.com>
2022-12-28 13:45:12 -0300

    config vercel

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://movies-db-sand.vercel.app/";
 
 const publicClient = axios.create({
   baseURL,
